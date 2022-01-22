2ad4acf806447bb7cb102514882dd3cc21d20157
luigi868686 <90272762+luigi868686@users.noreply.github.com>
2022-01-22 15:04:14 +0100

    Add files via upload

---
+++ file.js
@@ -0,0 +1,21 @@
+
+document.addEventListener("DOMContentLoaded", function(){
+  document.querySelector(".text-animation").style.color="white";
+  document.querySelector(".text-animation").style.borderColor="white";
+  document.querySelector(".text-animation").style.transform="translate(-50%, -50%) rotate(0deg) scale(1.2)";
+  document.querySelector(".text-animation").style.transition="all 5s";
+  document.querySelector(".text-animation").style.transitionDelay="4.5s";
+});
+
+
+window.addEventListener("load", function(){
+  document.querySelector(".fitness_h").style.transform="scale(1.1)";
+  document.querySelector(".fitness_h").style.transition="all 1s";
+  document.querySelector(".fitness_h").style.transitionDelay="all 2s";
+});
+
+
+// window.onload = function () {
+//   document.querySelector(".fitness_h").style.transform="scale(1)";
+//
+// }
