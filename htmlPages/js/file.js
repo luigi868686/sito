d586c85acc5931f533fa196dce091468a5ef479a
luigi868686 <90272762+luigi868686@users.noreply.github.com>
2022-02-20 23:07:37 +0100

    Add files via upload

--- htmlPages/js/file.js
+++ htmlPages/js/file.js
@@ -96,7 +96,71 @@ for (i=0; i<document.querySelectorAll(".hidden").length;i++) {
   document.querySelectorAll(".hidden")[i].addEventListener("click",function(){
       this.classList.toggle("active");
   });
-}
+};
+
+
+
+
+// inizio codice per muoversi tra i titoli
+listaIndice = document.querySelectorAll(".vaiAlPara");
+listaTitoli = document.querySelectorAll(".titoloPara");
+// trasformo il nodelist listaIndice in array:
+//in queso modo successivamente posso risalire all'index
+// di "this" ed usarlo come indice del nodelist dei titoli
+//dei paragrafi:
+const arrList = Array.from(listaIndice);
+//ora inizio il for-loop
+for (i=0; i<arrList.length; i++) {
+//ogni volta che clicco l'indice si esegue la funzione provaa
+listaIndice[i].addEventListener("click",provaa);
+//ecco la funzione provaa
+function provaa() {
+//ora riesco a sapere l'index dell'elemento che ho cliccato
+//nell'indice (perchè ho trasfromato il nodelist in array)
+h=arrList.indexOf(this);
+//qui ora mi imposto i titoli dei paragrafi e posso mettere
+//appunto la h selezionata
+offsetTopElement=document.querySelectorAll(".titoloPara")[h].offsetTop;
+//questa è l'alttezza fissa del navbar che copriva i titoloArticolo
+//perchè ha una position fixed ed il testo scorre sotto
+navbarHeight = document.getElementById("naviga").getBoundingClientRect().height;
+//e qui un pò di calcoli per mandare il titolo all'altezza giusta
+titleHeight=offsetTopElement-navbarHeight-20;
+//qui eseguo lo scroll fino alla misura individuata
+window.scrollTo(0,titleHeight);
+  }
+};
+// fine codice per muoversi tra i titoli.
+
+//inizio codice per andare dai titoli all'indice:
+tornaIndiceList = document.querySelectorAll(".tornaIndice");
+for (i=0; i<tornaIndiceList.length; i++) {
+  tornaIndiceList[i].addEventListener("click",function(){
+
+  offsetTopIndice=document.querySelector(".indice").offsetTop;
+  console.log(offsetTopIndice);
+  navbarHeight = document.getElementById("naviga").getBoundingClientRect().height;
+  heightIndice = offsetTopIndice-navbarHeight-40;
+  window.scrollTo(0,heightIndice);
+
+  })
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
